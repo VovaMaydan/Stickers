b8cddb1ed0485ae2e7aad45895e59280834714d6
VovaMaydan <58018479+VovaMaydan@users.noreply.github.com>
2022-04-09 07:40:10 +0300

    Add files via upload
    - Added Drop Menu;
    - Fixed Labels;
    - Added count how many stickers are left on each team + on the whole album.

---
+++ Stickers/LIST_OF_TEAMS.py
@@ -0,0 +1,95 @@
+TEAMS = [ { "minRange": 0, "maxRange": 68, "name": "Other"},
+{"minRange": 67, "maxRange": 85, 'name': "MC"},
+{"minRange": 85, "maxRange": 103, "name": "PSG"},
+{"minRange": 103, "maxRange": 121, "name": "Leipzig"},
+{"minRange": 121, "maxRange": 139, "name": "Brugge"},
+{"minRange": 139, "maxRange": 157, "name": "Atletico"},
+{"minRange": 157, "maxRange": 175, "name": "Liverpool"},
+{"minRange": 175, "maxRange": 193, "name": "Porto"},
+{"minRange": 193, "maxRange": 211, "name": "Milan"},
+{"minRange": 211, "maxRange": 229, "name": "Sporting"},
+{"minRange": 229, "maxRange": 247, "name": "Borussia"},
+{"minRange": 247, "maxRange": 265, "name": "Ajax"},
+{"minRange": 265, "maxRange": 283, "name": "Besiktas"},
+{"minRange": 283, "maxRange": 301, "name": "Inter"},
+{"minRange": 301, "maxRange": 319, "name": "Real"},
+{"minRange": 319, "maxRange": 337, "name": "Shakhtar"},
+{"minRange": 337, "maxRange": 355, "name": "Sheriff"},
+{"minRange": 355, "maxRange": 373, "name": "Bayern"},
+{"minRange": 373, "maxRange": 391, "name": "Barcelona"},
+{"minRange": 391, "maxRange": 409, "name": "Benfica"},
+{"minRange": 409, "maxRange": 427, "name": "Dynamo"},
+{"minRange": 427, "maxRange": 445, "name": "Villareal"},
+{"minRange": 445, "maxRange": 463, "name": "MU"},
+{"minRange": 463, "maxRange": 481, "name": "Atalanta"},
+{"minRange": 481, "maxRange": 499, "name": "YB"},
+{"minRange": 499, "maxRange": 517, "name": "Lille"},
+{"minRange": 517, "maxRange": 535, "name": "Sevilla"},
+{"minRange": 535, "maxRange": 553, "name": "Salzburg"},
+{"minRange": 553, "maxRange": 571, "name": "Wolfsburg"},
+{"minRange": 571, "maxRange": 589, "name": "Chelsea"},
+{"minRange": 589, "maxRange": 607, "name": "Juventus"},
+{"minRange": 607, "maxRange": 625, "name": "Zenit"},
+{"minRange": 625, "maxRange": 643, "name": "Malmo"},
+{"minRange": 673, "maxRange": 1000, "name": "Other2"}
+]
+
+TEAM_OPTIONS = [
+    "All",
+    "Ajax",
+    "Atalanta",
+    "Atletico",
+    "Barcelona",
+    "Bayern",
+    "Besiktas",
+    "Borussia",
+    "Brugge",
+    "Chelsea",
+    "Dynamo",
+    "Inter",
+    "Juventus",
+    "Leipzig",
+    "Lille",
+    "Liverpool",
+    "Malmo",
+    "MC",
+    "Milan",
+    "MU",
+    "Porto",
+    "PSG",
+    "Real",
+    "Salzburg",
+    "Sevilla",
+    "Shakhtar",
+    "Sheriff",
+    "Sporting",
+    "Villareal",
+    "Wolfsburg",
+    "YB"]
+
+
+    # print(teams[komanda]["minRange"])
+
+
+# Komandos = {
+#     "Other":"0",
+#     "MC":"1"
+# }
+#
+# team_name = "Other"
+# r = 0
+# if team_name in Komandos:
+#     x = Komandos[team_name]
+#     print(x)
+#     y = teams[int(x)]["minRange"]
+#     n = teams[int(x)]["maxRange"]
+#     for i in range(y, n):
+#         if str(i) in journal:
+#             pass
+#         elif str(i) not in journal:
+#             r += 1
+#     print(r)
+
+
+
+
